6c969d9579008af0c8fa189e036f01303c323e49
jung0930 <wjddlqkqh57@naver.com>
2022-08-09 21:40:34 +0900

    Feat: 레이아웃, 공통 컴포넌트 셋팅

--- src/App.js
+++ src/App.js
@@ -8,11 +8,44 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
+// COMONENTS
+import Mybutton from "./components/MyButton.js";
+import MyHeader from "./components/MyHeader.js";
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText="App"
+          leftChild={
+            <Mybutton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <Mybutton
+              text={"오른쪽 버튼"}
+              onClick={() => alert("오른쪽 클릭")}
+            />
+          }
+        />
         <h2>App.js</h2>
+
+        <Mybutton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+          type={"positive"}
+        />
+        <Mybutton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+          type={"negative"}
+        />
+        <Mybutton
+          text={"버튼"}
+          onClick={() => alert("버튼클릭")}
+          type={"defalut"}
+        />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
